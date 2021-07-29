923db927dac678150ba4c3a003ab3bd6dc70a091
Issac Abraham <issaca32@gmail.com>
2021-07-29 19:22:23 +0100

    gets all the videos from a playlist

--- js/Content.js
+++ js/Content.js
@@ -1,3 +1,14 @@
+function getTime(){
+    let videos = document.querySelectorAll('#playlist-items');
+
+    let timeMinutes = 0;
+    for (let video of videos){
+        let timeString = video.querySelector('.td-thumbnail-overlay-time-status-renderer').textContent;
+    }
+}
+
+
+
 
 let url = window.location.href;
 if (url.indexOf('list=') !== -1){
